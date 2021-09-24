3ff7df32f9a709a855eef7717b0fa3a2979e44f5
bilskirnirr <55932293+bilskirnirr@users.noreply.github.com>
2021-09-24 13:25:15 -0400

    Update __init__.py

--- Scrub/__init__.py
+++ Scrub/__init__.py
@@ -1,4 +1,4 @@
-from .wat import Scrub
+from .scrub import Scrub
 
 __red_end_user_data_statement__ = (
     "This cog does not persistently store data or metadata about users."
@@ -6,4 +6,4 @@ __red_end_user_data_statement__ = (
 
 
 def setup(bot):
-    bot.add_cog(Srcub(bot))
+    bot.add_cog(Scrub(bot))
